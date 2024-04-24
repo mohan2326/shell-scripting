d11a5c3f333b353ff3060852cf4e21a73f8ab336
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-04-24 19:32:57 -0400

    update

--- assignment-1.sh
+++ assignment-1.sh
@@ -31,27 +31,27 @@ else
 fi
 
 
-dnf install mysql-server -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL Server"
+# dnf install mysql-server -y &>>$LOGFILE
+# VALIDATE $? "Installing MySQL Server"
 
-systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "Enabling MySQL Server"
+# systemctl enable mysqld &>>$LOGFILE
+# VALIDATE $? "Enabling MySQL Server"
 
-systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "Starting MySQL Server"
+# systemctl start mysqld &>>$LOGFILE
+# VALIDATE $? "Starting MySQL Server"
 
-# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-# VALIDATE $? "Setting up root password"
+# # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# # VALIDATE $? "Setting up root password"
 
-#Below code will be useful for idempotent nature
-mysql -h db.mohansaivenna.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
-if [ $? -ne 0 ]
-then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    VALIDATE $? "MySQL Root password Setup"
-else
-    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
-fi
+# #Below code will be useful for idempotent nature
+# mysql -h db.mohansaivenna.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+# if [ $? -ne 0 ]
+# then
+#     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+#     VALIDATE $? "MySQL Root password Setup"
+# else
+#     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+# fi
 
 ###########
 #Now Installing Backend commands
