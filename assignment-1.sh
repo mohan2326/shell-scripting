566a07022bfa2376df496c4f5eeb68b48d07b9ac
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-04-24 21:05:12 -0400

    update

--- assignment-1.sh
+++ assignment-1.sh
@@ -4,10 +4,7 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -26,7 +23,6 @@ then
 else
     echo "You are super user."
 fi
-
 
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Server"
