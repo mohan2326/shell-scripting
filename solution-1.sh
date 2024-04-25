561562cd51db232ccdc41d7dfd1a406864654f21
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-04-24 20:30:56 -0400

    sol

---
+++ solution-1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if a file is provided as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 assignment-1.sh"
+    exit 1
+fi
+
+filename=$1
+
+# Check if the file exists
+if [ ! -f "$filename" ]; then
+    echo "File $filename not found."
+    exit 1
+fi
+
+# Call word_count.sh with the filename
+./word_count.sh "$filename"
